21f58f370698920a959bd5f2dd7bb67509176d06
preciousnjukia <preciousnjukia@gmail.com>
2023-11-27 10:15:20 +0300

    Assignment done

--- server/app.py
+++ server/app.py
@@ -14,14 +14,80 @@ app.json.compact = True
 migrate = Migrate(app, db)
 db.init_app(app)
 
-api = Api(app)
+with app.app_context():
+    db.create_all()
+    api = Api(app)
+
+
+class Index(Resource):
+
+    def get(self):
+
+        response_dict = {
+            "index": "Welcome to the Plants Store",
+        }
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+
+api.add_resource(Index, '/')
 
 class Plants(Resource):
-    pass
+
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+
+        return response
+
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+
+    def get(self, id):
+
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,26 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "image": self.image, "price": self.price}
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
